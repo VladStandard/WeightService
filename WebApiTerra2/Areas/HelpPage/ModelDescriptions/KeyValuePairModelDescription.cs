10d0bf13b5c37d8082b7f0276d84c854f4374d42
Damian Viktorovich Morozov <damian@morozov33.ru>
2021-11-23 10:43:42 +0300

    Removed WebApiTerra2

--- WebApiTerra2/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace terra2.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- WebApiTerra2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="terra2.WebApiApplication" Language="C#" %>
