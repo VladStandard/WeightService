6495726fff9e8435eed34acb150422f2856849ec
Damian Viktorovich Morozov <damian@morozov33.ru>
2021-11-23 12:35:32 +0300

    Removed SsccWebAPI

--- SsccWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SsccWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- SsccWebAPI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SsccWebAPI.WebApiApplication" Language="C#" %>
