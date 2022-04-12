9fadc8f06aa4a9650b7d111537e05b4872a2ecc2
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-04-12 12:33:15 +0300

    Updated BlazorAppTemplate202112

--- BlazorAppTemplate202112/Program.cs
+++ BlazorAppTemplate202112/Program.cs
@@ -2,10 +2,12 @@ using BlazorAppTemplate202112;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped<Radzen.DialogService>();
+builder.Services.AddScoped<Radzen.NotificationService>();
+builder.Services.AddScoped<Radzen.TooltipService>();
+builder.Services.AddScoped<Radzen.ContextMenuService>();
 await builder.Build().RunAsync();
