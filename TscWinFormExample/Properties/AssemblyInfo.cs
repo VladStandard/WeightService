7b87e0fd9e7fc2b91f64616dfb04dcbd61ef2b81
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-05-27 15:58:42 +0300

    Updated version to 0.6.750

--- TscWinFormExample/Properties/AssemblyInfo.cs
+++ TscWinFormExample/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      組建編號
 //      修訂編號
 //
-[assembly: AssemblyVersion("0.6.745.0")]
-[assembly: AssemblyFileVersion("0.6.745.0")]
+[assembly: AssemblyVersion("0.6.750.0")]
+[assembly: AssemblyFileVersion("0.6.750.0")]
