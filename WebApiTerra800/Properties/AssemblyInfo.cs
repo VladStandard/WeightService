c4785ad920c4581006baefaf70a1afd3bb69ca3f
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-07-07 08:36:37 +0300

    Updated version to v0.6.875

--- WebApiTerra800/Properties/AssemblyInfo.cs
+++ WebApiTerra800/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.6.873.0")]
-[assembly: AssemblyFileVersion("0.6.873.0")]
+[assembly: AssemblyVersion("0.6.875.0")]
+[assembly: AssemblyFileVersion("0.6.875.0")]
