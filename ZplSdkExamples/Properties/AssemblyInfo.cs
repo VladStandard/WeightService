94bbeb77d93c5c91c93cd3ff1c2f7f1a87bacd95
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-09-28 13:08:24 +0300

    Updated version to v0.7.215

--- ZplSdkExamples/Properties/AssemblyInfo.cs
+++ ZplSdkExamples/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.210.0")]
-[assembly: AssemblyFileVersion("0.7.210.0")]
+[assembly: AssemblyVersion("0.7.215.0")]
+[assembly: AssemblyFileVersion("0.7.215.0")]
