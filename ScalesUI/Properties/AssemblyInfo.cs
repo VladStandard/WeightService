0457935691a1748be1a6d1ff7cee200af4c92ca3
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-10-10 16:53:58 +0300

    Updated version to v0.7.281

--- ScalesUI/Properties/AssemblyInfo.cs
+++ ScalesUI/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.280.0")]
-[assembly: AssemblyFileVersion("0.7.280.0")]
+[assembly: AssemblyVersion("0.7.281.0")]
+[assembly: AssemblyFileVersion("0.7.281.0")]
 
