ee067bc4ea8dd6b6747620bedb2cab5dfbca21ed
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-10-12 09:10:57 +0300

    Updated version to v0.7.295

--- ScalesUI/Properties/AssemblyInfo.cs
+++ ScalesUI/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.290.0")]
-[assembly: AssemblyFileVersion("0.7.290.0")]
+[assembly: AssemblyVersion("0.7.295.0")]
+[assembly: AssemblyFileVersion("0.7.295.0")]
 
--- ZplSdkExamples/Properties/AssemblyInfo.cs
+++ ZplSdkExamples/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.290.0")]
-[assembly: AssemblyFileVersion("0.7.290.0")]
+[assembly: AssemblyVersion("0.7.295.0")]
+[assembly: AssemblyFileVersion("0.7.295.0")]
