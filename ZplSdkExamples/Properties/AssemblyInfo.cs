4d8e4c332c211f0113bb003d079c2c5fca83c61a
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-11-21 17:27:33 +0300

    Updated version to v0.7.515

--- ZplSdkExamples/Properties/AssemblyInfo.cs
+++ ZplSdkExamples/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.7.505.0")]
-[assembly: AssemblyFileVersion("0.7.505.0")]
+[assembly: AssemblyVersion("0.7.515.0")]
+[assembly: AssemblyFileVersion("0.7.515.0")]
