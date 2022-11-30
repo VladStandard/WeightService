cf7ae84af869c8e063fd391ff71825c2dc4fb034
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-11-30 16:47:54 +0300

    Updated version to v0.7.550

--- SerialPortExchangeTool/Properties/AssemblyInfo.cs
+++ SerialPortExchangeTool/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ebd60157-fb56-442b-9434-bb583babdeee")]
-[assembly: AssemblyVersion("0.7.545.0")]
-[assembly: AssemblyFileVersion("0.7.545.0")]
+[assembly: AssemblyVersion("0.7.550.0")]
+[assembly: AssemblyFileVersion("0.7.550.0")]
