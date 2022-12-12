4619dd28ab9c74227810370aadb073348b510dba
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-12-12 09:30:57 +0300

    #WS-T-576 Added ItemNomenclatureGroup

---
+++ BlazorDeviceControl/Razors/ItemComponents/References1C/ItemNomenclatureGroup.razor.cs
@@ -0,0 +1,34 @@
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using BlazorCore.Razors;
+
+namespace BlazorDeviceControl.Razors.ItemComponents.References1C;
+
+public partial class ItemNomenclatureGroup : RazorComponentItemBase<NomenclatureGroupModel>
+{
+	#region Public and private fields, properties, constructor
+
+	//
+
+	#endregion
+
+	#region Public and private methods
+
+	protected override void OnParametersSet()
+	{
+		RunActionsParametersSet(new()
+		{
+			() =>
+			{
+                SqlItemCast = DataAccess.GetItemNotNullable<NomenclatureGroupModel>(IdentityUid);
+                if (SqlItemCast.IdentityIsNew)
+	                SqlItem = SqlItemNew<NomenclatureGroupModel>();
+
+				ButtonSettings = new(false, false, false, false, false, true, true);
+			}
+		});
+	}
+
+	#endregion
+}
