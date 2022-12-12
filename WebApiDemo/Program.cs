4d87d5dd7f76b0179161aad0da7aa16855d2b869
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-12-12 16:43:15 +0300

    Updated WebApiDemo

--- WebApiDemo/Program.cs
+++ WebApiDemo/Program.cs
@@ -10,16 +10,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+	options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+	options.RoutePrefix = string.Empty;
+});
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
