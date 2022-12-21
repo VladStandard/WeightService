3c8af889b1097aa3bb1da2395debf773c005b283
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-12-21 15:06:46 +0300

    #WS-T-639 #DataCore Updated DataCore/Sql/TableScaleModels

--- DataCore/Sql/TableScaleModels/Plus/PluValidator.cs
+++ DataCore/Sql/TableScaleModels/Plus/PluValidator.cs
@@ -51,29 +51,36 @@ public class PluValidator : SqlTableValidator<PluModel>
         RuleFor(item => item.Itf14)
             //.NotEmpty()
             .NotNull();
-        RuleFor(item => item.UpperThreshold)
-            .NotNull()
-            .GreaterThanOrEqualTo(0)
-            .LessThanOrEqualTo(100)
-            .GreaterThanOrEqualTo(item => item.LowerThreshold)
-            .GreaterThanOrEqualTo(item => item.NominalWeight);
-        RuleFor(item => item.NominalWeight)
-            .NotNull()
-            .GreaterThanOrEqualTo(0)
-            .GreaterThanOrEqualTo(item => item.LowerThreshold)
-            .LessThanOrEqualTo(100)
-            .LessThanOrEqualTo(item => item.UpperThreshold);
-        RuleFor(item => item.LowerThreshold)
-            .NotNull()
-            .GreaterThanOrEqualTo(0)
-            .LessThanOrEqualTo(100)
-            .LessThanOrEqualTo(item => item.UpperThreshold)
-            .LessThanOrEqualTo(item => item.NominalWeight);
-        RuleFor(item => item.IsCheckWeight)
+		RuleFor(item => item.IsCheckWeight)
             .NotNull();
         RuleFor(item => item.Nomenclature)
             .NotEmpty()
             .NotNull()
             .SetValidator(new NomenclatureValidator());
-    }
+        // Fix for 0 values.
+        RuleFor(item => item.UpperThreshold)
+	        .NotNull()
+	        .GreaterThanOrEqualTo(0)
+	        .LessThanOrEqualTo(100);
+        RuleFor(item => item.NominalWeight)
+	        .NotNull()
+	        .GreaterThanOrEqualTo(0)
+	        .LessThanOrEqualTo(100);
+        RuleFor(item => item.LowerThreshold)
+	        .NotNull()
+	        .GreaterThanOrEqualTo(0)
+	        .LessThanOrEqualTo(100);
+        RuleFor(item => item.UpperThreshold)
+	        .GreaterThanOrEqualTo(item => item.LowerThreshold)
+	        .GreaterThanOrEqualTo(item => item.NominalWeight)
+	        .When(item => item.UpperThreshold > 0 && item.NominalWeight > 0 && item.LowerThreshold > 0);
+		RuleFor(item => item.NominalWeight)
+	        .GreaterThanOrEqualTo(item => item.LowerThreshold)
+	        .LessThanOrEqualTo(item => item.UpperThreshold)
+			.When(item => item.UpperThreshold > 0 && item.NominalWeight > 0 && item.LowerThreshold > 0);
+		RuleFor(item => item.LowerThreshold)
+	        .LessThanOrEqualTo(item => item.UpperThreshold)
+	        .LessThanOrEqualTo(item => item.NominalWeight)
+			.When(item => item.UpperThreshold > 0 && item.NominalWeight > 0 && item.LowerThreshold > 0);
+	}
 }
