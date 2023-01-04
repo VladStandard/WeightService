7ef128ecfba64411543939155ef966126b3afe64
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-01-04 22:26:45 +0300

    #Release #ScalesUI

--- TscPrintDemoWpf/Properties/AssemblyInfo.cs
+++ TscPrintDemoWpf/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.8.30.0")]
-[assembly: AssemblyFileVersion("0.8.30.0")]
+[assembly: AssemblyVersion("0.8.40.0")]
+[assembly: AssemblyFileVersion("0.8.40.0")]
