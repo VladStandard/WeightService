08dc0403c3292c2a1956917e6e6b3991e7c3a178
BaggerFast <riosha3@gmail.com>
2023-01-11 16:47:41 +0300

    UPDATE: add Name field to PluNestingFkModel, change version to 0.8.100.0

--- ConsoleQuartzExample/Properties/AssemblyInfo.cs
+++ ConsoleQuartzExample/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("03f82b97-71c5-4e00-aa9e-54dc604089c0")]
-[assembly: AssemblyVersion("0.8.100.0")]
-[assembly: AssemblyFileVersion("0.8.100.0")]
+[assembly: AssemblyVersion("0.8.110.0")]
+[assembly: AssemblyFileVersion("0.8.110.0")]
