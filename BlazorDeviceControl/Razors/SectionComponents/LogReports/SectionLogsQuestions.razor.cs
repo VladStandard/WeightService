435e338249557aa88744d48f91e54c5dd484e3fc
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-01-12 11:57:57 +0300

    Added LogReports into #BlazorDeviceControl

---
+++ BlazorDeviceControl/Razors/SectionComponents/LogReports/SectionLogsQuestions.razor.cs
@@ -0,0 +1,39 @@
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using BlazorCore.Razors;
+using DataCore.Sql.TableScaleModels.Logs;
+using DataCore.Sql.TableScaleModels.LogsTypes;
+
+namespace BlazorDeviceControl.Razors.SectionComponents.Logs;
+
+public partial class SectionLogsQuestions : RazorComponentSectionBase<LogModel, LogTypeModel>
+{
+	#region Public and private fields, properties, constructor
+
+	public SectionLogsQuestions()
+	{
+		SqlCrudConfigSection.IsGuiShowItemsCount = true;
+        SqlCrudConfigSection.IsGuiShowFilterMarked = true;
+    }
+
+    #endregion
+
+    #region Public and private methods
+
+    protected override void OnParametersSet()
+    {
+        RunActionsParametersSet(new()
+        {
+            () =>
+            {
+	            SqlCrudConfigModel sqlCrudConfig = SqlCrudConfigUtils.GetCrudConfig(
+		            nameof(LogTypeModel.Number), (byte)LogTypeEnum.Question,
+		            SqlCrudConfigItem.IsResultShowMarked, SqlCrudConfigItem.IsResultShowOnlyTop);
+				SqlItem = BlazorAppSettings.DataAccess.GetItemNotNullable<LogTypeModel>(sqlCrudConfig);
+            }
+        });
+    }
+
+    #endregion
+}
