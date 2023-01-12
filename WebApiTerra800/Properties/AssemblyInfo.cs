a871990f687db9441c818438cfeacf5aa669c412
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-01-12 16:39:03 +0300

    #Version update to v0.8.130

--- WebApiTerra800/Properties/AssemblyInfo.cs
+++ WebApiTerra800/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9731f2f4-3d6d-481f-a97a-70eb58b78b3b")]
-[assembly: AssemblyVersion("0.8.120.0")]
-[assembly: AssemblyFileVersion("0.8.120.0")]
+[assembly: AssemblyVersion("0.8.130.0")]
+[assembly: AssemblyFileVersion("0.8.130.0")]
