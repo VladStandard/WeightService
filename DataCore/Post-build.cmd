e47bc7d7f30b36f9bd0bdeae396fe5b7ad547541
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-01-31 15:16:14 +0300

    Updated #DataCore/Post-build.cmd

--- DataCore/Post-build.cmd
+++ DataCore/Post-build.cmd
@@ -1,2 +1,32 @@
-xcopy "appsettings.Debug.json" "\\palych\Install\VSSoft\appsettings\" /Y /S /Q /F /R /V
-xcopy "appsettings.Release.json" "\\palych\Install\VSSoft\appsettings\" /Y /S /Q /F /R /V
+@echo off
+cls
+setlocal
+echo ------------------------------------------------------------
+echo ---               DataCore/Post-build.cmd                ---
+echo ------------------------------------------------------------
+
+set "host=palych"
+call :job "%host%"
+endlocal
+exit /b 0
+
+:job <host>
+echo --- [ ] Check the connection to the host [%~1] ---
+call :ping "%~1" && call :isConnectGood "%~1" || call :isConnectBad "%~1"
+echo ------------------------------------------------------------
+exit /b %ErrorLevel%
+
+:ping <host>
+echo --- [ ] The host %~1 is ping ---
+ping "%~1" -n 1 | find "TTL=" >nul
+exit /b %ErrorLevel%
+
+:isConnectGood <host>
+echo --- [v] The host [%~1] is online ---
+xcopy "appsettings.Debug.json" "\\palych\Install\VSSoft\appsettings\" /Y /S /Q /F /R /V >nul
+echo --- [v] xcopy "appsettings.Debug.json" is complete ---
+xcopy "appsettings.Release.json" "\\palych\Install\VSSoft\appsettings\" /Y /S /Q /F /R /V >nul
+echo --- [v] xcopy "appsettings.Release.json" is complete ---
+
+:isConnectBad <host>
+echo --- [x] The host [%~1] is offline ---
