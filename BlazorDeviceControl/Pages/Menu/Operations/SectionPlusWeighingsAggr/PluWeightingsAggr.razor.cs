6b228bba2dea576c561d5b1a4c4c0c96e2aec07b
BaggerFast <riosha3@gmail.com>
2023-04-05 14:01:52 +0300

    UPDATE: update PluWeightingsAggr - DeviceControl COMPLETE: #WS-T-932

--- BlazorDeviceControl/Pages/Menu/Operations/SectionPlusWeighingsAggr/PluWeightingsAggr.razor.cs
+++ BlazorDeviceControl/Pages/Menu/Operations/SectionPlusWeighingsAggr/PluWeightingsAggr.razor.cs
@@ -1,61 +1,81 @@
-// // This is an independent project of an individual developer. Dear PVS-Studio, please check it.
-// // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
-//
-// using DataCore.Sql.TableScaleFkModels.Aggregations;
-// using DataCore.Sql.TableScaleFkModels.PlusWeighingsFks;
-//
-// namespace BlazorDeviceControl.Pages.Menu.Operations.PlusWeighingsAggrSection;
-//
-// public sealed partial class PluWeightingsAggr : RazorComponentSectionBase<WeithingFactSummaryModel>
-// {
-//     #region Public and private fields, properties, constructor
-//
-//     private bool IsAddPlu { get; set; }
-//     private List<PluAggrModel> PluAggrs { get; set; }
-//
-//     public PluWeightingsAggr()
-//     {
-//         PluAggrs = new();
-//         ButtonSettings = new(false, false, false, false, false, false, false);
-//     }
-//
-// 	#endregion
-//
-// 	#region Public and private methods
-//
-//     protected override void SetSqlSectionCast()
-//     {
-//         object[] objects = IsAddPlu 
-//             ? DataAccess.GetArrayObjectsNotNullable(SqlQueries.DbScales.Tables.PluWeighings.GetWeighingsAggrWithPlu(
-//                 SqlCrudConfigSection.IsResultShowOnlyTop ? DataAccess.JsonSettings.Local.SelectTopRowsCount : 0))
-//             : DataAccess.GetArrayObjectsNotNullable(SqlQueries.DbScales.Tables.PluWeighings.GetWeighingsAggrWithoutPlu(
-//                 SqlCrudConfigSection.IsResultShowOnlyTop ? DataAccess.JsonSettings.Local.SelectTopRowsCount : 0));
-//         PluAggrs = new();
-//         foreach (object obj in objects)
-//         {
-//             if (IsAddPlu)
-//             {
-//                 if (obj is object[] { Length: 5 } item)
-//                 {
-//                     PluAggrs.Add(new(Convert.ToDateTime(item[0]),
-//                         Convert.ToInt32(item[1]), Convert.ToString(item[2]) ?? string.Empty,
-//                         Convert.ToString(item[3]) ?? string.Empty, Convert.ToString(item[4]) ?? string.Empty)
-//                     );
-//                 }
-//             }
-//             else
-//             {
-//                 if (obj is object[] { Length: 4 } item)
-//                 {
-//                     PluAggrs.Add(new(Convert.ToDateTime(item[0]),
-//                         Convert.ToInt32(item[1]), Convert.ToString(item[2]) ?? string.Empty,
-//                         Convert.ToString(item[3]) ?? string.Empty)
-//                     );
-//                 }
-//             }
-//         }
-//         //SqlSectionCast = items;
-//     }
-//
-//     #endregion
-// }
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using DataCore.Sql.Core.Helpers;
+using DataCore.Sql.TableScaleFkModels.Aggregations;
+
+namespace BlazorDeviceControl.Pages.Menu.Operations.SectionPlusWeighingsAggr;
+
+public sealed partial class PluWeightingsAggr: LayoutComponentBase
+{
+    #region Public and private fields, properties, constructor
+    
+    private bool IsShowPlu { get; set; }
+    
+    private string SqlListCountResult => $"{LocaleCore.Strings.ItemsCount}: {PluWeightAggrs.Count:### ### ###}";
+    private List<PluAggrModel> PluWeightAggrs { get; set; }
+
+    public PluWeightingsAggr()
+    {
+        PluWeightAggrs = new();
+    }
+
+	#endregion
+
+	#region Public and private methods
+    
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (!firstRender)
+        {
+            base.OnAfterRender(firstRender);
+            return;
+        }
+        GetSectionData();
+    }
+    
+    private void GetSectionData()
+    {
+        if (IsShowPlu)
+            LoadAggrWithPlu();
+        else
+            LoadAggrWithoutPlu();
+        StateHasChanged();
+    }
+
+    private void LoadAggrWithoutPlu()
+    {
+        PluWeightAggrs = new();
+        object[] sql_objects = DataAccessHelper.Instance.GetArrayObjectsNotNullable(
+            SqlQueries.DbScales.Tables.PluWeighings.GetWeighingsAggrWithoutPlu(200));
+        foreach (object obj in sql_objects)
+        {
+            if (obj is object[] { Length: 4 } item)
+            {
+                PluWeightAggrs.Add(new(Convert.ToDateTime(item[0]),
+                    Convert.ToInt32(item[1]), Convert.ToString(item[2]) ?? string.Empty,
+                    Convert.ToString(item[3]) ?? string.Empty)
+                );
+            }
+        }
+    }
+
+    private void LoadAggrWithPlu()
+    {
+        PluWeightAggrs = new();
+        object[] sql_objects = DataAccessHelper.Instance.GetArrayObjectsNotNullable(
+            SqlQueries.DbScales.Tables.PluWeighings.GetWeighingsAggrWithPlu(200));
+        foreach (object obj in sql_objects)
+        {
+            if (obj is object[] { Length: 5 } item)
+            {
+                PluWeightAggrs.Add(new(Convert.ToDateTime(item[0]),
+                    Convert.ToInt32(item[1]), Convert.ToString(item[2]) ?? string.Empty,
+                    Convert.ToString(item[3]) ?? string.Empty, Convert.ToString(item[4]) ?? string.Empty)
+                );
+            }
+        }
+    }
+    
+    #endregion
+}
