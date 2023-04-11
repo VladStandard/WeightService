42e1a1f9038a66ce6944fbf999959e68f90b63c3
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-04-11 14:54:08 +0300

    Updated #WsWebApiScales #ScalesUI

--- WsWebApiScales/Controllers/PlusCharacteristicsController.cs
+++ WsWebApiScales/Controllers/PlusCharacteristicsController.cs
@@ -35,14 +35,12 @@ public sealed class PlusCharacteristicsController : WsControllerBase
         [FromHeader(Name = "host")] string host = "", [FromHeader(Name = "accept")] string version = "")
     {
         DateTime requestStampDt = DateTime.Now;
-        //ContentResult result = GetAcceptVersion(version) switch
-        //{
-        //    AcceptVersion.V2 => GetContentResult(() => NewResponse1cIsNotFound(
-        //        $"Version {version} {LocaleCore.WebService.IsNotFound}!", format, isDebug, SessionFactory), format),
-        //    _ => GetContentResult(() => WsPlusCharacteristics.NewResponse1cPluCharacteristics(xml, format, isDebug, SessionFactory), format)
-        //};
-        ContentResult result = GetContentResult(() => NewResponse1cIsNotFound(LocaleCore.WebService.Underdevelopment, 
-            format, isDebug, SessionFactory), format);
+        ContentResult result = GetAcceptVersion(version) switch
+        {
+            AcceptVersion.V2 => GetContentResult(() => NewResponse1cIsNotFound(
+                $"Version {version} {LocaleCore.WebService.IsNotFound}!", format, isDebug, SessionFactory), format),
+            _ => GetContentResult(() => WsPlusCharacteristics.NewResponse1cPluCharacteristics(xml, format, isDebug, SessionFactory), format)
+        };
         LogWebServiceFk(nameof(WsWebApiScales), WsWebServiceUrls.SendNomenclaturesCharacteristics,
             requestStampDt, xml, result.Content ?? string.Empty, format, host, version).ConfigureAwait(false);
         return result;
