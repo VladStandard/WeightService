e456477dd039687583ecbce9aa969034e1a07dfe
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-04-17 12:25:37 +0300

    Moved deprecated projects

--- Deprecated/ResourcesVs/GlobalUsings.cs
+++ Deprecated/ResourcesVs/GlobalUsings.cs
@@ -2,5 +2,5 @@
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
 // Global using directives
 
-global using WsLocalization.Enums;
-global using WsLocalization.Models;
+global using WsLocalizationCore.Enums;
+global using WsLocalizationCore.Models;
