9e0a01ccec8fcb492f5540db3425668214681c4d
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-04-21 10:30:53 +0300

    #Version update to v0.9.250

--- Demo/ConsoleQuartzDemo/Properties/AssemblyInfo.cs
+++ Demo/ConsoleQuartzDemo/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("03f82b97-71c5-4e00-aa9e-54dc604089c0")]
-[assembly: AssemblyVersion("0.9.240.0")]
-[assembly: AssemblyFileVersion("0.9.240.0")]
+[assembly: AssemblyVersion("0.9.250.0")]
+[assembly: AssemblyFileVersion("0.9.250.0")]
