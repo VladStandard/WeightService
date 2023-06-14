73a1661d25b202bd97fc67169741438557296830
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-06-14 12:08:02 +0300

    #Version update to v0.9.580

--- Demo/TscWpfExample/Properties/AssemblyInfo.cs
+++ Demo/TscWpfExample/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("0.9.570.0")]
-[assembly: AssemblyFileVersion("0.9.570.0")]
+[assembly: AssemblyVersion("0.9.580.0")]
+[assembly: AssemblyFileVersion("0.9.580.0")]
