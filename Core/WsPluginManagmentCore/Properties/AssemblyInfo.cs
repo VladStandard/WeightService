94667f6885da502fb14334960c1f63fa5285cfce
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-06-15 15:58:19 +0300

    #Version update to v0.9.590

--- Core/WsPluginManagmentCore/Properties/AssemblyInfo.cs
+++ Core/WsPluginManagmentCore/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.580.0")]
-[assembly: AssemblyFileVersion("0.9.580.0")]
+[assembly: AssemblyVersion("0.9.590.0")]
+[assembly: AssemblyFileVersion("0.9.590.0")]
