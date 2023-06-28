f4126dd922d54a77dd8ce7b79ba7a240ef4ede3f
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-06-28 10:18:24 +0300

    #Version update to v0.0.710

--- Clients/ScalesUI/Properties/AssemblyInfo.cs
+++ Clients/ScalesUI/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("7be2ce0f-d6d7-4851-b0f1-955381371454")]
-[assembly: AssemblyVersion("0.9.700.0")]
-[assembly: AssemblyFileVersion("0.9.700.0")]
+[assembly: AssemblyVersion("0.9.710.0")]
+[assembly: AssemblyFileVersion("0.9.710.0")]
