71d63fcee08f9e66689a9114193c4bbc6ca119dc
BaggerFast <riosha3@gmail.com>
2023-07-14 16:21:59 +0300

    ADD: WsStorage tests for GetList in ViewRefModelsRepositories

---
+++ Tests/WsStorageCoreTests/ViewRefModels/PluNestings/ViewPluNestingRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using WsStorageCore.ViewRefModels.PluNestings;
+
+namespace WsStorageCoreTests.ViewRefModels.PluNestings;
+
+public class ViewPluNestingRepositoryTests
+{
+    private WsSqlViewPluNestingRepository ViewPluNestingRepository = WsSqlViewPluNestingRepository.Instance;
+    
+    [Test]
+    public void GetList()
+    {
+        WsTestsUtils.DataTests.AssertAction(() =>
+        {
+            List<WsSqlViewPluNestingModel> items = ViewPluNestingRepository.GetList();
+            Assert.That(items.Any(), Is.True);
+            WsTestsUtils.DataTests.PrintTopRecords(items, 10);
+        }, false, new() { WsEnumConfiguration.DevelopVS, WsEnumConfiguration.ReleaseVS });
+    }
+}
