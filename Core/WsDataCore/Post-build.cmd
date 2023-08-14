4c313b20bbe75c8b53618a4672915bbf1f314330
Damian Viktorovich Morozov <damian@morozov33.ru>
2023-08-14 09:36:02 +0300

    #WS-T-1100 Post-build.cmd

--- Core/WsDataCore/Post-build.cmd
+++ Core/WsDataCore/Post-build.cmd
@@ -4,33 +4,36 @@ setlocal
 echo ------------------------------------------------------------
 echo ---              WsDataCore/Post-build.cmd               ---
 echo ------------------------------------------------------------
-
 set "host=palych"
 call :job "%host%"
-endlocal
-exit /b 0
+goto :end
 
-:job <host>
-echo --- [ ] Check the connection to the host [%~1] ---
+:job
+echo [ ] Start job
+echo [ ] Check the connection to the host [%~1]
 call :ping "%~1" && call :isConnectGood "%~1" || call :isConnectBad "%~1"
 echo ------------------------------------------------------------
-exit /b %ErrorLevel%
+exit %ErrorLevel%
 
 :ping <host>
-echo --- [ ] The host %~1 is ping ---
 ping "%~1" -n 1 | find "TTL=" >nul
 exit /b %ErrorLevel%
 
 :isConnectGood <host>
-echo --- [v] The host [%~1] is online ---
 xcopy "appsettings.DevelopAleksandrov.json" "\\palych\Install\VSSoft\appsettings\" /Y /S /Q /F /R /V >nul
 xcopy "appsettings.DevelopMorozov.json" "\\palych\Install\VSSoft\appsettings\" /Y /S /Q /F /R /V >nul
 xcopy "appsettings.DevelopVS.json" "\\palych\Install\VSSoft\appsettings\" /Y /S /Q /F /R /V >nul
-echo --- [v] xcopy "appsettings.Develop*.json" is complete ---
+echo [v] The files "appsettings.Develop*.json" has been successfully copied.
 xcopy "appsettings.ReleaseAleksandrov.json" "\\palych\Install\VSSoft\appsettings\" /Y /S /Q /F /R /V >nul
 xcopy "appsettings.ReleaseMorozov.json" "\\palych\Install\VSSoft\appsettings\" /Y /S /Q /F /R /V >nul
 xcopy "appsettings.ReleaseVS.json" "\\palych\Install\VSSoft\appsettings\" /Y /S /Q /F /R /V >nul
-echo --- [v] xcopy "appsettings.Release*.json" is complete ---
+echo [v] The files "appsettings.Release*.json" has been successfully copied.
+goto :end
 
 :isConnectBad <host>
-echo --- [x] The host [%~1] is offline ---
+echo [x] The host [%~1] is offline
+
+:end
+echo [ ] End job
+endlocal
+exit 0
