645740143ad4e9e98dc3ca5c2eb2ef48436b4d52
BaggerFast <riosha3@gmail.com>
2023-09-19 14:44:52 +0300

    RELEASE: from 1.0.1 -> 1.0.2

--- Clients/ScalesUI/Properties/AssemblyInfo.cs
+++ Clients/ScalesUI/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("7be2ce0f-d6d7-4851-b0f1-955381371454")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
--- Tools/SerialPortExchangeTool/Properties/AssemblyInfo.cs
+++ Tools/SerialPortExchangeTool/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ebd60157-fb56-442b-9434-bb583babdeee")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
