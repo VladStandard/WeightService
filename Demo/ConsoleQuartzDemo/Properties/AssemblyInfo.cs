03dcaa4fbdd124608031dd6e320f43f6083dd1e5
BaggerFast <riosha3@gmail.com>
2023-09-21 15:52:16 +0300

    RELEASE: 1.0.2 -> 1.0.3

--- Demo/ConsoleQuartzDemo/Properties/AssemblyInfo.cs
+++ Demo/ConsoleQuartzDemo/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("03f82b97-71c5-4e00-aa9e-54dc604089c0")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
