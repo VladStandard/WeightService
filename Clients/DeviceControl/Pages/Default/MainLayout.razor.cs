e02e59258f3aeafd7aeb28c27486fbbcac1d7074
BaggerFast <riosha3@gmail.com>
2023-10-23 15:00:26 +0300

    DELETE: ThemeService from DeviceControl

--- Clients/DeviceControl/Pages/Default/MainLayout.razor.cs
+++ Clients/DeviceControl/Pages/Default/MainLayout.razor.cs
@@ -1,12 +1,9 @@
-using Microsoft.AspNetCore.Http.Extensions;
-
 namespace DeviceControl.Pages.Default;
 
 public partial class MainLayout : LayoutComponentBase
 {
     [Inject] private WsUserService UserService { get; set; } = default!;
     [Inject] private NavigationManager UriHelper { get; set; } = default!;
-    [Inject] private WsThemeService ThemeService { get; set; } = default!;
     private ClaimsPrincipal? User { get; set; }
     private static BlazorAppSettingsHelper BlazorAppSettings => BlazorAppSettingsHelper.Instance;
     private static string VerBlazor => $"v{WsBlazorCoreUtils.GetLibVersion()}";
@@ -21,12 +18,6 @@ public partial class MainLayout : LayoutComponentBase
         BlazorAppSettings.Memory.OpenAsync().ConfigureAwait(false);
         await base.OnInitializedAsync();
     }
-
-    private void ChangeTheme(object value)
-    {
-        string url = UriHelper.GetUriWithQueryParameter("theme", $"{value}");
-        UriHelper.NavigateTo(url, true);
-    }
-
+    
     #endregion
 }
