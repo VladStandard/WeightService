bf8240f34a330ef29ab16e3f04d158b0339f3e67
BaggerFast <riosha3@gmail.com>
2023-10-27 18:23:30 +0300

    UPDATE: minor refactor

--- DataAccess/WsStorageCore/Entities/SchemaScale/PlusNestingFks/WsSqlPluNestingFkRepository.cs
+++ DataAccess/WsStorageCore/Entities/SchemaScale/PlusNestingFks/WsSqlPluNestingFkRepository.cs
@@ -3,17 +3,6 @@ namespace WsStorageCore.Entities.SchemaScale.PlusNestingFks;
 
 public sealed class WsSqlPluNestingFkRepository : WsSqlTableRepositoryBase<WsSqlPluNestingFkEntity>
 {
-    private WsSqlBoxRepository ContextBox { get; } = new();
-    private WsSqlPluRepository ContextPlu { get; } = new();
-    
-    public WsSqlPluNestingFkEntity GetNewItem()
-    {
-        WsSqlPluNestingFkEntity item = SqlCore.GetItemNewEmpty<WsSqlPluNestingFkEntity>();
-        item.Box = ContextBox.GetNewItem();
-        item.Plu = ContextPlu.GetNewItem();
-        return item;
-    }
-
     public WsSqlViewPluNestingModel GetNewView() => new();
 
     public IEnumerable<WsSqlPluNestingFkEntity> GetEnumerable(WsSqlCrudConfigModel sqlCrudConfig)
@@ -45,17 +34,6 @@ public sealed class WsSqlPluNestingFkRepository : WsSqlTableRepositoryBase<WsSql
         sqlCrudConfig.AddFilters(new() {
             SqlRestrictions.Equal(nameof(WsSqlPluNestingFkEntity.IsDefault), true),
             SqlRestrictions.EqualFk(nameof(WsSqlPluTemplateFkEntity.Plu), plu)
-        });
-        return SqlCore.GetItemByCrud<WsSqlPluNestingFkEntity>(sqlCrudConfig);
-    }
-
-    public WsSqlPluNestingFkEntity GetByAttachmentsCountAndPlu(WsSqlPluEntity plu, short attachmentsCount)
-    {
-        WsSqlCrudConfigModel sqlCrudConfig = WsSqlCrudConfigFactory.GetCrudAll();
-        
-        sqlCrudConfig.AddFilters(new() {
-            SqlRestrictions.Equal(nameof(WsSqlPluNestingFkEntity.BundleCount), attachmentsCount),
-            SqlRestrictions.EqualFk(nameof(WsSqlPluNestingFkEntity.Plu), plu)
         });
         return SqlCore.GetItemByCrud<WsSqlPluNestingFkEntity>(sqlCrudConfig);
     }
--- DataAccess/WsStorageCore/Utils/WsSqlQueriesDiags.cs
+++ DataAccess/WsStorageCore/Utils/WsSqlQueriesDiags.cs
@@ -4,108 +4,6 @@ public static class WsSqlQueriesDiags
 {
     public static class Views
     {
-        public static string GetViewLogsDevices(string appName, string deviceName, WsSqlEnumTimeInterval timeInterval, int records)
-        {
-            string where = timeInterval switch
-            {
-                WsSqlEnumTimeInterval.All => WsSqlQueries.GetWhereAppDeviceName(appName, deviceName),
-                WsSqlEnumTimeInterval.Today => WsSqlQueries.GetWhereAppDeviceNameDay(appName, deviceName),
-                WsSqlEnumTimeInterval.Month => WsSqlQueries.GetWhereAppNameMonth(appName, deviceName),
-                WsSqlEnumTimeInterval.Year => WsSqlQueries.GetWhereAppNameYear(appName, deviceName),
-                _ => WsSqlQueries.GetWhereEmpty(),
-            };
-            return WsSqlQueries.TrimQuery($@"
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-SELECT {WsSqlQueries.GetTopRecords(records)}
-	 [UID]
-	,[CREATE_DT]
-	,[LINE_NAME]
-	,[DEVICE_NAME]
-	,[APP_NAME]
-	,[VERSION]
-	,[FILE_NAME]
-	,[CODE_LINE]
-	,[MEMBER]
-	,[LOG_TYPE]
-	,[MESSAGE]
-FROM [DIAG].[VIEW_LOGS_DEVICES] {where}
-ORDER BY [CREATE_DT] DESC;");
-        }
-        
-        public static string GetViewLogsDevicesAggr(string appName, string deviceName, WsSqlEnumTimeInterval timeInterval, int records)
-        {
-            string where = timeInterval switch
-            {
-                WsSqlEnumTimeInterval.All => WsSqlQueries.GetWhereAppDeviceName(appName, deviceName),
-                WsSqlEnumTimeInterval.Today => WsSqlQueries.GetWhereAppDeviceNameDay(appName, deviceName),
-                WsSqlEnumTimeInterval.Month => WsSqlQueries.GetWhereAppNameMonth(appName, deviceName),
-                WsSqlEnumTimeInterval.Year => WsSqlQueries.GetWhereAppNameYear(appName, deviceName),
-                _ => WsSqlQueries.GetWhereEmpty(),
-            };
-            return timeInterval switch
-            {
-                WsSqlEnumTimeInterval.Month => WsSqlQueries.TrimQuery($@"
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-SELECT {WsSqlQueries.GetTopRecords(records)}
-	 CONVERT(VARCHAR(7), [CREATE_DT], 120) [CREATE_DT]
-    ,[DEVICE_NAME]
-    ,[APP_NAME]
-    ,[LINE_NAME]
-    ,[LOG_TYPE]
-    ,SUM([COUNT]) [COUNT]
-FROM [DIAG].[VIEW_LOGS_DEVICES_AGGR] {where}
-GROUP BY CONVERT(VARCHAR(7), [CREATE_DT], 120), [DEVICE_NAME], [APP_NAME], [LINE_NAME], [LOG_TYPE]
-ORDER BY [CREATE_DT], [LOG_TYPE];"),
-                WsSqlEnumTimeInterval.Year => WsSqlQueries.TrimQuery($@"
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-SELECT {WsSqlQueries.GetTopRecords(records)}
-	 CONVERT(VARCHAR(4), [CREATE_DT], 120) [CREATE_DT]
-    ,[DEVICE_NAME]
-    ,[APP_NAME]
-    ,[LINE_NAME]
-    ,[LOG_TYPE]
-    ,SUM([COUNT]) [COUNT]
-FROM [DIAG].[VIEW_LOGS_DEVICES_AGGR] {where}
-GROUP BY CONVERT(VARCHAR(4), [CREATE_DT], 120), [DEVICE_NAME], [APP_NAME], [LINE_NAME], [LOG_TYPE]
-ORDER BY [CREATE_DT], [LOG_TYPE];"),
-                _ => WsSqlQueries.TrimQuery($@"
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-SELECT {WsSqlQueries.GetTopRecords(records)}
-	 [CREATE_DT]
-	,[DEVICE_NAME]
-	,[APP_NAME]
-	,[LINE_NAME]
-	,[LOG_TYPE]
-    ,[COUNT]
-FROM [DIAG].[VIEW_LOGS_DEVICES_AGGR] {where}
-ORDER BY [CREATE_DT] DESC, [LOG_TYPE];"),
-            };
-        }
-        
-        public static string GetViewLogsMemory(string appName, string deviceName, WsSqlEnumTimeInterval timeInterval, int records)
-        {
-            string where = timeInterval switch
-            {
-                WsSqlEnumTimeInterval.All => WsSqlQueries.GetWhereAppDeviceName(appName, deviceName),
-                WsSqlEnumTimeInterval.Today => WsSqlQueries.GetWhereAppDeviceNameDay(appName, deviceName),
-                WsSqlEnumTimeInterval.Month => WsSqlQueries.GetWhereAppNameMonth(appName, deviceName),
-                WsSqlEnumTimeInterval.Year => WsSqlQueries.GetWhereAppNameYear(appName, deviceName),
-                _ => WsSqlQueries.GetWhereEmpty(),
-            };
-            return WsSqlQueries.TrimQuery($@"
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-SELECT {WsSqlQueries.GetTopRecords(records)}
-	 [UID]
-	,[CREATE_DT]
-	,[APP_NAME]
-	,[DEVICE_NAME]
-	,[SCALE_NAME]
-	,[SIZE_APP_MB]
-	,[SIZE_FREE_MB]
-FROM [diag].[VIEW_LOGS_MEMORIES] {where}
-ORDER BY [CREATE_DT] DESC;");
-        }
-        
         public static string GetViewTablesSizes(int records = 0) => WsSqlQueries.TrimQuery($@"
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
 SELECT {WsSqlQueries.GetTopRecords(records)}
@@ -268,20 +166,6 @@ WHERE
 {currentLine}
 order by [CREATE_DT] DESC");
         }
-
-        public static string GetWebLogs(int records) => WsSqlQueries.TrimQuery($@"
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
-select {WsSqlQueries.GetTopRecords(records)}
-	 [UID]
-	,[CREATE_DT]
-	,[REQUEST_URL]
-	,[REQUEST_COUNT_ALL]
-	,[RESPONSE_COUNT_SUCCESS]
-	,[RESPONSE_COUNT_ERROR]
-	,[APP_NAME]
-	,[APP_VERSION]
-FROM [diag].[VIEW_LOGS_WEB_SERVICES]
-order by [CREATE_DT] DESC");
 
         public static string GetBarcodes(int records, WsSqlEnumIsMarked isMarked) => WsSqlQueries.TrimQuery($@"
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED;
