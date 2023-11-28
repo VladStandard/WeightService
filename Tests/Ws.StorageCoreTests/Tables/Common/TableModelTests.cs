8a31f854848e9c02a0bf638f667982239ebec06d
BaggerFast <riosha3@gmail.com>
2023-11-28 12:12:55 +0300

    FIX: WsStorageTests

--- Tests/Ws.StorageCoreTests/Tables/Common/TableModelTests.cs
+++ Tests/Ws.StorageCoreTests/Tables/Common/TableModelTests.cs
@@ -3,30 +3,12 @@
 public class TableModelTests<TItem> where TItem : SqlEntityBase, new()
 {
     [Test]
-    public virtual void Model_ToString()
-    {
-        Assert.DoesNotThrow(() =>
-        {
-            // Arrange.
-            TItem item = new();
-            SqlEntityBase baseItem = new();
-            // Act.
-            string itemString = item.ToString();
-            string baseString = baseItem.ToString();
-            TestContext.WriteLine($"{nameof(itemString)}: {itemString}");
-            TestContext.WriteLine($"{nameof(baseString)}: {baseString}");
-            // Assert.
-            Assert.That(itemString, Is.Not.EqualTo(baseString));
-        });
-    }
-
-    [Test]
     public virtual void Model_EqualsNew()
     {
         Assert.DoesNotThrow(() =>
         {
             TItem item = new();
-            Assert.That(item, Is.EqualTo(new()));
+            Assert.That(item, Is.EqualTo(new TItem()));
         });
     }
 }
