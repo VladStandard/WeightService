61f14564f5102cd00e7b83b080dd07b9a31a535a
BaggerFast <riosha3@gmail.com>
2023-12-06 13:33:18 +0300

    DELETE: ScaleUi, Ws.LabelCore, Ws.PrinterCore, Ws.MassaCore

--- Domain/Ws.MassaCore/Models/SerialPortEventArgs.cs
+++
@@ -1,7 +0,0 @@
-namespace Ws.MassaCore.Models;
-
-public class SerialPortEventArgs : EventArgs
-{
-	public SerialPort SerialPort { get; set; } = new();
-	public byte[] ReceivedBytes { get; set; } = new byte[0];
-}
--- Domain/Ws.PrinterCore/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-global using System;
-global using System.Collections.Generic;
-global using System.Linq;
-global using Ws.StorageCore.Models;
-global using Ws.StorageCore.Utils;
-global using Zebra.Sdk.Comm;
-global using Zebra.Sdk.Printer;
-global using ZebraPrinterStatus = Zebra.Sdk.Printer.PrinterStatus;
