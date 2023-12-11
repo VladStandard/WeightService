db2679786482768041d18841f4684b9e1d879576
Ap73MKa <artyom.vlasov.05@mail.ru>
2023-12-11 10:17:38 +0300

    FIX: overflow

--- Presentation/DeviceControl/Utils/BlazorCoreUtils.cs
+++ Presentation/DeviceControl/Utils/BlazorCoreUtils.cs
@@ -1,22 +1,18 @@
-using System.Diagnostics;
 using System.Reflection;
 
 namespace DeviceControl.Utils;
 
 public static class BlazorCoreUtils
 {
-    #region Public and private methods
-
     public static string GetLibVersion()
     {
-        FileVersionInfo fieVersionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
-        if (string.IsNullOrEmpty(fieVersionInfo.FileVersion))
-            return string.Empty;
-        
-        string result = fieVersionInfo.FileVersion;
-        int endIndex = result.LastIndexOf(".0", StringComparison.InvariantCultureIgnoreCase);
-        return endIndex != -1 ? result[..endIndex] : result;
-    }
+        AssemblyInformationalVersionAttribute? versionAttribute = Assembly.GetEntryAssembly()?
+            .GetCustomAttribute<AssemblyInformationalVersionAttribute>();
 
-    #endregion
+        if (versionAttribute == null) return "undefined";
+
+        string fullVersion = versionAttribute.InformationalVersion;
+        int endIndex = fullVersion.LastIndexOf(".0", StringComparison.InvariantCultureIgnoreCase);
+        return endIndex != -1 ? fullVersion[..endIndex] : fullVersion;
+    }
 }
