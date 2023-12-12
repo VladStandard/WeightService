61da68918a810e7d8dcbd01ce4ba8d7770a08083
Ap73MKa <artyom.vlasov.05@mail.ru>
2023-12-12 08:33:05 +0300

    TEST: disable zooming

--- Presentation/ScalesHybrid/MainPage.xaml.cs
+++ Presentation/ScalesHybrid/MainPage.xaml.cs
@@ -8,8 +8,7 @@ public partial class MainPage : ContentPage
     }
     private void Bwv_BlazorWebViewInitialized(object sender, Microsoft.AspNetCore.Components.WebView.BlazorWebViewInitializedEventArgs e)
     {
-    #if WINDOWS
+        e.WebView.CoreWebView2.Settings.IsPinchZoomEnabled = false;
         e.WebView.CoreWebView2.Settings.IsZoomControlEnabled = false;
-    #endif
     }
 }
