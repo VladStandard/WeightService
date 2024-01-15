d53c8847afe03c79bf921a38abeb37aab94031a5
BaggerFast <riosha3@gmail.com>
2024-01-15 16:25:51 +0300

    FIX: SqlUserEntity LoginDt validation

--- DataAccess/Ws.StorageCore/Entities/SchemaRef/Users/SqlUserValidator.cs
+++ DataAccess/Ws.StorageCore/Entities/SchemaRef/Users/SqlUserValidator.cs
@@ -1,15 +1,9 @@
-using Ws.Shared.Validators;
-using Ws.StorageCore.Entities.SchemaRef.Hosts;
-
 namespace Ws.StorageCore.Entities.SchemaRef.Users;
 
 public sealed class SqlUserValidator : SqlTableValidator<SqlUserEntity>
 {
     public SqlUserValidator(bool isCheckIdentity) : base(isCheckIdentity)
     {
-        RuleFor(item => item.LoginDt)
-            .NotEmpty()
-            .NotNull();
         RuleFor(item => item.Name)
             .NotEmpty()
             .NotNull();
