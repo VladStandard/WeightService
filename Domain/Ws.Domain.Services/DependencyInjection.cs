1df172aab1a3938d2960488df802ae3d1b1e00df
BaggerFast <riosha3@gmail.com>
2024-02-12 11:17:39 +0300

    UPDATE: add check password to PalletMan - ScalesHybrid

--- Domain/Ws.Domain.Services/DependencyInjection.cs
+++ Domain/Ws.Domain.Services/DependencyInjection.cs
@@ -43,7 +43,7 @@ public static class DependencyInjection
         services.AddScoped<ITemplateService, TemplateService>();
         services.AddScoped<ITemplateResourceService, TemplateResourceService>();
         services.AddScoped<IWarehouseService, WarehouseService>();
-        services.AddSingleton<IUserService, UserService>();
-        services.AddSingleton<IPalletManService, PalletManService>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IPalletManService, PalletManService>();
     }
 }
