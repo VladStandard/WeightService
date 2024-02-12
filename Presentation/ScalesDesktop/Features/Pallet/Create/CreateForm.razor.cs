a54b35ff16a7217881b6e163fedbe80b1ea29723
BaggerFast <riosha3@gmail.com>
2024-02-12 12:03:25 +0300

    UPDATE: GetLinePiecePlus to CreatePalletForm

--- Presentation/ScalesDesktop/Features/Pallet/Create/CreateForm.razor.cs
+++ Presentation/ScalesDesktop/Features/Pallet/Create/CreateForm.razor.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components.Forms;
 using ScalesDesktop.Services;
 using Ws.Domain.Models.Entities.Ref1c;
 using Ws.Domain.Models.Entities.Scale;
+using Ws.Domain.Services.Features.Line;
 using Ws.Domain.Services.Features.Plu;
 
 namespace ScalesDesktop.Features.Pallet.Create;
@@ -14,12 +15,12 @@ public sealed partial class CreateForm : ComponentBase
     [Inject] private INotificationService NotificationService { get; set; } = null!;
     [Inject] private LineContext LineContext { get; set; } = null!;
     [Inject] private IPluService PluService { get; set; } = null!;
-
+    [Inject] private ILineService LineService{ get; set; } = null!;
     [SupplyParameterFromForm] private PalletCreateModel FormModel { get; set; } = new();
 
     private IEnumerable<PluNestingEntity> PluNestings { get; set; } = [];
 
-    private IEnumerable<PluEntity> GetAllPlus() => PluService.GetAll();
+    private IEnumerable<PluEntity> GetAllPlus() => LineService.GetLinePiecePlus(LineContext.Line);
 
     private void SetPluNestings()
     {
