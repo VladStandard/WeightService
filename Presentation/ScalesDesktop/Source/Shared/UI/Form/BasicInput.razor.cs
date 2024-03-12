cb200ba20d159c40a150d8ff5e684973028919c5
Ap73MKa <artyom.vlasov.05@mail.ru>
2024-03-12 15:27:33 +0300

    FIX: data grid virtualize ADD: custom abstraction for fluent inputs

---
+++ Presentation/ScalesDesktop/Source/Shared/UI/Form/BasicInput.razor.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components;
+
+namespace ScalesDesktop.Source.Shared.UI.Form;
+
+public abstract class BasicInput<TValue>: ComponentBase
+{
+    [Parameter] public TValue? Value { get; set; }
+    [Parameter] public EventCallback<TValue> ValueChanged { get; set; }
+    [Parameter] public string Class { get; set; } = string.Empty;
+    [Parameter] public bool Disabled { get; set; }
+    [Parameter] public bool AutoFocus { get; set; }
+    [Parameter] public string Placeholder { get; set; } = string.Empty;
+    [Parameter] public bool ReadOnly { get; set; }
+    [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object> AdditionalAttributes { get; set; } = new();
+    
+    protected async Task OnValueChanged() => await ValueChanged.InvokeAsync(Value);
+}
