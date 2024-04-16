0bb9aa9d044c61c6a4a541e0d24ff32d9e937fac
Ap73MKa <artyom.vlasov.05@mail.ru>
2024-04-16 15:06:40 +0300

    ADD: stepper components UPDATE: stages in pallet create form

---
+++ Presentation/ScalesDesktop/Source/Features/PalletCreate/PalletCreateModel.cs
@@ -0,0 +1,14 @@
+using Ws.Domain.Models.Entities.Ref1c;
+using Ws.Domain.Models.Entities.Scale;
+
+namespace ScalesDesktop.Source.Features.PalletCreate;
+
+public class PalletCreateModel
+{
+    public PluEntity? Plu { get; set; }
+    public PluNestingEntity? Nesting { get; set; }
+    public int Count { get; set; } = 1;
+    public decimal PalletWeight { get; set; }
+    public short Kneading { get; set; } = 1;
+    public DateTime? CreateDt { get; set; } = DateTime.Now;
+}
