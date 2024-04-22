bdfed34e74346e655025bdbedc5527213d4a0260
BaggerFast <riosha3@gmail.com>
2024-04-22 16:35:50 +0300

    UPDATE: migrate to net8.0

--- Src/App/Services/Ws.Labels.Service/Features/RenderLabel/RenderLabelService.cs
+++ Src/App/Services/Ws.Labels.Service/Features/RenderLabel/RenderLabelService.cs
@@ -1,12 +1,41 @@
-using System.Drawing;
-using System.Drawing.Imaging;
+using System.Net.Http.Headers;
 using System.Text;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Processing;
 using Ws.Labels.Service.Features.RenderLabel.Exceptions;
 
 namespace Ws.Labels.Service.Features.RenderLabel;
 
 internal class RenderLabelService : IRenderLabelService
 {
+
+    public async Task<string> GetZplPreviewBase64(string zpl)
+    {
+        const string requestUrl = "http://api.labelary.com/v1/printers/12dpmm/labels/2.36x5.9/0/";
+        byte[] zplBytes = Encoding.UTF8.GetBytes(zpl);
+
+        using HttpClient client = new();
+        using HttpContent content = new ByteArrayContent(zplBytes);
+        content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+        try
+        {
+            HttpResponseMessage response = await client.PostAsync(requestUrl, content);
+            if (!response.IsSuccessStatusCode) throw new RenderLabelException();
+
+            byte[] imageBytes = await GetImageBytesFromResponse(response);
+            return ConvertImageToBase64(imageBytes);
+        }
+        catch (Exception ex) when (ex is HttpRequestException
+                                       or ObjectDisposedException
+                                       or ArgumentException
+                                       or IOException)
+        {
+            throw new RenderLabelException();
+        }
+    }
+
     #region Private
 
     private static async Task<byte[]> GetImageBytesFromResponse(HttpResponseMessage response)
@@ -19,37 +48,14 @@ internal class RenderLabelService : IRenderLabelService
     private static string ConvertImageToBase64(byte[] imageBytes)
     {
         using MemoryStream imageStream = new(imageBytes);
-        using Image originalImage = Image.FromStream(imageStream);
-        originalImage.RotateFlip(RotateFlipType.Rotate90FlipNone);
+        using Image image = Image.Load(imageStream);
+
+        image.Mutate(x => x.Rotate(90));
 
         using MemoryStream rotatedStream = new();
-        originalImage.Save(rotatedStream, ImageFormat.Png);
+        image.Save(rotatedStream, new PngEncoder());
         return Convert.ToBase64String(rotatedStream.ToArray());
     }
 
     #endregion
-
-    public async Task<string> GetZplPreviewBase64(string zpl)
-    {
-        const string requestUrl = "http://api.labelary.com/v1/printers/12dpmm/labels/2.36x5.9/0/";
-        byte[] zplBytes = Encoding.UTF8.GetBytes(zpl);
-
-        using HttpClient client = new();
-        using HttpContent content = new ByteArrayContent(zplBytes);
-        content.Headers.ContentType = new("application/x-www-form-urlencoded");
-
-        try
-        {
-            HttpResponseMessage response = await client.PostAsync(requestUrl, content);
-            if (!response.IsSuccessStatusCode) throw new RenderLabelException();
-
-            byte[] imageBytes = await GetImageBytesFromResponse(response);
-            return ConvertImageToBase64(imageBytes);
-        }
-        catch (Exception ex) when (ex is HttpRequestException or ObjectDisposedException or ArgumentException
-                                       or IOException)
-        {
-            throw new RenderLabelException();
-        }
-    }
 }
