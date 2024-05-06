dfa86a1b00ba69efd08bdd39f3be397e580b4d73
Ap73MKa <artyom.vlasov.05@mail.ru>
2024-05-06 12:22:43 +0300

    ADD: barcode validator UPDATE: packages versions, move barcode preview from component STYLE: move common styles to ws components, fix form divider took as two columns in small screen mode

---
+++ Src/App/Clients/DeviceControl/Source/Features/BarcodeConfigurator/BarcodeError.cs
@@ -0,0 +1,8 @@
+namespace DeviceControl.Source.Features.BarcodeConfigurator;
+
+public class BarcodeError
+{
+    public string Barcode { get; set; } = string.Empty;
+    public string ItemName { get; set; } = string.Empty;
+    public string Error { get; set; } = string.Empty;
+}
