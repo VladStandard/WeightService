ebd92b65bf975ded760114f97e3638b223b7c4c9
Ap73MKa <artyom.vlasov.05@mail.ru>
2024-06-06 12:37:12 +0300

    UPDATE: nuget packages versions ADD: handle Initializing state of scales

--- Src/App/Clients/ScalesDesktop/Source/Shared/Services/PrinterService.cs
+++ Src/App/Clients/ScalesDesktop/Source/Shared/Services/PrinterService.cs
@@ -53,7 +53,7 @@ public class PrinterService(IDispatcher dispatcher): IDisposable
 
     public void StopPolling() => Printer.StopStatusPolling();
 
-    public void StartPolling(short interval = 30) => Printer.StartStatusPolling(interval);
+    public void StartPolling(ushort interval = 30) => Printer.StartStatusPolling(interval);
 
     public void Disconnect()
     {
