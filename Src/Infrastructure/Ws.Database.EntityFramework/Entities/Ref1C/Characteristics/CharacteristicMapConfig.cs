a431bb573465d168d1db15769685064f6a299358
BaggerFast <riosha3@gmail.com>
2024-06-19 13:09:47 +0300

    UPDATE: add Characteristics IEntityTypeConfiguration (remove annotations)

--- Src/Infrastructure/Ws.Database.EntityFramework/Entities/Ref1C/Characteristics/CharacteristicMapConfig.cs
+++ Src/Infrastructure/Ws.Database.EntityFramework/Entities/Ref1C/Characteristics/CharacteristicMapConfig.cs
@@ -7,9 +7,45 @@ internal class CharacteristicMapConfig : IEntityTypeConfiguration<Characteristic
 {
     public void Configure(EntityTypeBuilder<CharacteristicEntity> builder)
     {
+        #region Base
+
+        builder.ToTable(SqlTables.Characteristics, SqlSchemas.Ref1C);
+
+        builder.HasIndex(e => new { e.PluId, e.BoxId, e.BundleCount })
+            .HasDatabaseName($"UQ_{SqlTables.Characteristics}__UNIQ")
+            .IsUnique();
+
+        #endregion
+
+        #region FK
+
+        builder.Property(e => e.PluId)
+            .HasColumnName("PLU_UID");
+
         builder.HasOne<PluEntity>()
             .WithMany()
             .HasForeignKey(characteristic => characteristic.PluId)
-            .HasConstraintName("FK_CHARACTERISTICS_PLUS_PLU_UID");
+            .HasConstraintName($"FK_{SqlTables.Characteristics}__PLU")
+            .IsRequired();
+
+        builder.Property(e => e.BoxId)
+            .HasColumnName("BOX_UID");
+
+        builder.HasOne(e => e.Box)
+            .WithMany()
+            .HasForeignKey(characteristic => characteristic.BoxId)
+            .HasConstraintName($"FK_{SqlTables.Characteristics}__BOX")
+            .IsRequired();
+
+        #endregion
+
+        builder.Property(e => e.Name)
+            .HasColumnName(SqlColumns.Name)
+            .HasColumnType("varchar(64)")
+            .IsRequired();
+
+        builder.Property(e => e.BundleCount)
+            .HasColumnName("BUNDLE_COUNT")
+            .IsRequired();
     }
 }
---
+++ Src/Infrastructure/Ws.Database.EntityFramework/Migrations/20240619100919_Update_rework_CharacteristicsTable.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ws.Database.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_rework_CharacteristicsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CHARACTERISTICS_BOXES_BOX_UID",
+                schema: "REF_1C",
+                table: "CHARACTERISTICS");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CHARACTERISTICS_PLUS_PLU_UID",
+                schema: "REF_1C",
+                table: "CHARACTERISTICS");
+
+            migrationBuilder.RenameIndex(
+                name: "UQ_CHARACTERISTICS_UNIQ",
+                schema: "REF_1C",
+                table: "CHARACTERISTICS",
+                newName: "UQ_CHARACTERISTICS__UNIQ");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CHARACTERISTICS__BOX",
+                schema: "REF_1C",
+                table: "CHARACTERISTICS",
+                column: "BOX_UID",
+                principalSchema: "REF_1C",
+                principalTable: "BOXES",
+                principalColumn: "UID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CHARACTERISTICS__PLU",
+                schema: "REF_1C",
+                table: "CHARACTERISTICS",
+                column: "PLU_UID",
+                principalSchema: "REF_1C",
+                principalTable: "PLUS",
+                principalColumn: "UID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CHARACTERISTICS__BOX",
+                schema: "REF_1C",
+                table: "CHARACTERISTICS");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CHARACTERISTICS__PLU",
+                schema: "REF_1C",
+                table: "CHARACTERISTICS");
+
+            migrationBuilder.RenameIndex(
+                name: "UQ_CHARACTERISTICS__UNIQ",
+                schema: "REF_1C",
+                table: "CHARACTERISTICS",
+                newName: "UQ_CHARACTERISTICS_UNIQ");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CHARACTERISTICS_BOXES_BOX_UID",
+                schema: "REF_1C",
+                table: "CHARACTERISTICS",
+                column: "BOX_UID",
+                principalSchema: "REF_1C",
+                principalTable: "BOXES",
+                principalColumn: "UID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CHARACTERISTICS_PLUS_PLU_UID",
+                schema: "REF_1C",
+                table: "CHARACTERISTICS",
+                column: "PLU_UID",
+                principalSchema: "REF_1C",
+                principalTable: "PLUS",
+                principalColumn: "UID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
