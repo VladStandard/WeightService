b6c292a214b4febfdc0278ef1744b09bcbb25197
BaggerFast <riosha3@gmail.com>
2024-06-19 12:32:51 +0300

    UPDATE: add Warehouse IEntityTypeConfiguration (remove annotations)

--- Src/Infrastructure/Ws.Database.EntityFramework/Entities/Ref/Warehouses/WarehouseEntity.cs
+++ Src/Infrastructure/Ws.Database.EntityFramework/Entities/Ref/Warehouses/WarehouseEntity.cs
@@ -2,18 +2,10 @@ using Ws.Database.EntityFramework.Entities.Ref.ProductionSites;
 
 namespace Ws.Database.EntityFramework.Entities.Ref.Warehouses;
 
-[Table(SqlTables.Warehouses, Schema = SqlSchemas.Ref)]
-[Index(nameof(Name), Name = $"UQ_{SqlTables.Warehouses}_NAME", IsUnique = true)]
-[Index(nameof(Uid1C), Name = $"UQ_{SqlTables.Warehouses}_UID_1C", IsUnique = true)]
 public sealed class WarehouseEntity : EfEntityBase
 {
-    [Column(SqlColumns.Uid1C)]
     public Guid Uid1C { get; set; }
-
-    [Column(SqlColumns.Name), StringLength(32)]
     public string Name { get; set; } = string.Empty;
-
-    [ForeignKey("PRODUCTION_SITE_UID")]
     public ProductionSiteEntity ProductionSite { get; set; } = new();
 
     #region Date
