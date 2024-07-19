8074a7fdceee622d23611ce0aa29695f6556b809
Ap73MKa <artyom.vlasov.05@mail.ru>
2024-07-19 10:47:16 +0300

    REPLACE: data source in references 1c pages

--- Src/Apps/Web/DeviceControl/Source/Shared/Services/References1CEndpoints.cs
+++ Src/Apps/Web/DeviceControl/Source/Shared/Services/References1CEndpoints.cs
@@ -1,5 +1,6 @@
 using Phetch.Core;
 using Ws.DeviceControl.Models;
+using Ws.DeviceControl.Models.Dto.References1C.Brands;
 using Ws.DeviceControl.Models.Dto.Shared;
 
 namespace DeviceControl.Source.Shared.Services;
@@ -8,5 +9,17 @@ public class References1CEndpoints(IWebApi webApi)
 {
     public ParameterlessEndpoint<PackageDto[]> BoxesEndpoint { get; } = new(
         webApi.GetBoxes,
+        options: new() { DefaultStaleTime = TimeSpan.FromMinutes(1) });
+
+    public ParameterlessEndpoint<PackageDto[]> BundlesEndpoint { get; } = new(
+        webApi.GetBundles,
+        options: new() { DefaultStaleTime = TimeSpan.FromMinutes(1) });
+
+    public ParameterlessEndpoint<PackageDto[]> ClipsEndpoint { get; } = new(
+        webApi.GetClips,
+        options: new() { DefaultStaleTime = TimeSpan.FromMinutes(1) });
+
+    public ParameterlessEndpoint<BrandDto[]> BrandsEndpoint { get; } = new(
+        webApi.GetBrands,
         options: new() { DefaultStaleTime = TimeSpan.FromMinutes(1) });
 }
--- Src/Apps/Web/Ws.DeviceControl.Models/IWebApi.cs
+++ Src/Apps/Web/Ws.DeviceControl.Models/IWebApi.cs
@@ -1,5 +1,6 @@
 using Refit;
 using Ws.DeviceControl.Models.Dto.Database;
+using Ws.DeviceControl.Models.Dto.References1C.Brands;
 using Ws.DeviceControl.Models.Dto.Shared;
 
 namespace Ws.DeviceControl.Models;
@@ -17,4 +18,22 @@ public interface IWebApi
 
     [Get("/boxes/{Uid}")]
     Task<PackageDto> GetBoxByUid(Guid uid);
+
+    [Get("/brands")]
+    Task<BrandDto[]> GetBrands();
+
+    [Get("/brands/{Uid}")]
+    Task<BrandDto> GetBrandByUid(Guid uid);
+
+    [Get("/clips")]
+    Task<PackageDto[]> GetClips();
+
+    [Get("/clips/{Uid}")]
+    Task<PackageDto> GetClipByUid(Guid uid);
+
+    [Get("/bundles")]
+    Task<PackageDto[]> GetBundles();
+
+    [Get("/bundles/{Uid}")]
+    Task<PackageDto> GetBundleByUid(Guid uid);
 }
