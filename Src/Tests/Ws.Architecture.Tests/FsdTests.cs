c6d6ea1f9a1c919a039de5d3ab4f8db25540ae3c
BaggerFast <riosha3@gmail.com>
2024-08-27 12:33:20 +0300

    FIX: FsdTests build

--- Src/Tests/Ws.Architecture.Tests/FsdTests.cs
+++ Src/Tests/Ws.Architecture.Tests/FsdTests.cs
@@ -2,6 +2,13 @@ using System.Reflection;
 
 namespace Ws.Architecture.Tests;
 
+public static class DllConstants
+{
+    public const string DeviceControl = @"..\..\..\..\..\Apps\Web\DeviceControl\bin\Develop_x64\net8.0-windows10.0.19041.0\DeviceControl.dll";
+    public const string ScalesDesktop = @"..\..\..\..\..\Apps\Desktop\ScalesDesktop\bin\Develop_x64\net8.0-windows10.0.19041.0\win10-x64\ScalesDesktop.dll";
+    public const string ScalesMobile = @"..\..\..\..\..\Apps\Mobile\ScalesMobile\bin\Develop_x64\net8.0-android34.0\ScalesMobile.dll";
+}
+
 public class FsdTests
 {
     public static readonly TheoryData<string, string> TestData = new()
@@ -13,11 +20,11 @@ public class FsdTests
 
     [Theory]
     [MemberData(nameof(TestData))]
-    public void Shared_Should_HaveNoDependencies(string projectName, string assemblyPath)
+    public void Shared_Should_HaveNoDependencies(string projectName, string assembly)
     {
-        Types? types = Types.InAssembly(Assembly.LoadFrom(assemblyPath));
+        Types types = Types.InAssembly(Assembly.LoadFrom(assembly));
 
-        TestResult? result = types
+        TestResult result = types
             .That()
             .ResideInNamespace($"{projectName}.Source.Shared")
             .ShouldNot()
@@ -33,11 +40,11 @@ public class FsdTests
 
     [Theory]
     [MemberData(nameof(TestData))]
-    public void Features_Should_OnlyReference_Shared(string projectName, string assemblyPath)
+    public void Features_Should_OnlyReference_Shared(string projectName, string assembly)
     {
-        Types? assembly = Types.InAssembly(Assembly.LoadFrom(assemblyPath));
+        Types types = Types.InAssembly(Assembly.LoadFrom(assembly));
 
-        TestResult? result = assembly
+        TestResult result = types
             .That()
             .ResideInNamespace($"{projectName}.Source.Features")
             .ShouldNot()
@@ -51,11 +58,11 @@ public class FsdTests
 
     [Theory]
     [MemberData(nameof(TestData))]
-    public void Widgets_Should_OnlyReference_Shared_And_Features(string projectName, string assemblyPath)
+    public void Widgets_Should_OnlyReference_Shared_And_Features(string projectName, string assembly)
     {
-        Types? assembly = Types.InAssembly(Assembly.LoadFrom(assemblyPath));
+        Types types = Types.InAssembly(Assembly.LoadFrom(assembly));
 
-        TestResult? result = assembly
+        TestResult result = types
             .That()
             .ResideInNamespace($"{projectName}.Source.Widgets")
             .ShouldNot()
